f6e1ab134f66d1407bd29f6d796045c36982758b
Wells-Alex <wells_@outlook.com>
2025-06-22 18:34:04 +0200

    1.0

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,2 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task1 {
+    public static void main(String[] args) {
+        String regex = "^((\\d{3}-\\d{3}-\\d{4}) | (\\(\\d{3}\\) \\d{3}-\\d{4}))$";
+        Pattern pattern = Pattern.compile(regex);
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/java/file.txt"))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                line = line.trim();
+
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.matches()) {
+                    System.out.println(line);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+    }
 }
